bcf6bac9f8644562a922f7468ae933d46a627077
Tobias Nagelmaier <tobias.nagelmaier@htlstp.at>
2019-12-12 09:09:04 +0100

    modified:   main.c

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main(void)
     printf("Hello world!\n");
     printf("Dies ist eine Aenderung.\n");
     printf("noch eine Aenderung\n");
-    printf("was passier?\n")
+    printf("was passiert?\n");
     return 0;
 }
