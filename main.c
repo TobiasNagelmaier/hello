7fc33da429b0d77b8f2c06a82b92d73fba3d1670
Tobias Nagelmaier <tobias.nagelmaier@htlstp.at>
2020-01-09 08:13:30 +0100

    modified:   main.c

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 
 int main(void)
 {
+    int i;
     printf("Hello world!\n");
     printf("Dies ist eine Aenderung.\n");
     printf("noch eine Aenderung\n");
     printf("was passiert?\n");
+    for(i = 0; i < 1; i++)
+        printf("%d", i);
     return 0;
 }
